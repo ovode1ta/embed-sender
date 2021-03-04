7b4ff6cf3d9e9ebbaf66457c0339a50ca55b4028
cgd <71853433+cgdisgood@users.noreply.github.com>
2021-03-04 09:14:21 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+import pyperclip
+import sys
+from termcolor import colored, cprint
+import subprocess
+from pynput.keyboard import Key, Controller
+
+
+with open('config.json') as json_file:
+    data = json.load(json_file)
+
+    token = data.get('token')
+
+
+
+def main():
+
+    title = input("Enter the title of the embed to send: ")
+
+    description = input("Now enter the description of the embed: ")
+
+    channelid = input("Finally, enter the id of the channel to send the message in (Doesn't work in DMs): ")
+
+    curlCommand = 'curl -X POST https://discord.com/api/v8/channels/' + channelid + '/messages -H "Authorization: ' + token + '" -H "Content-type: application/json" -d "{\\\"content\\\":\\\"\\\",\\\"embed\\\":{\\\"title\\\":\\\"' + title + '\\\", \\\"description\\\": \\\"' + description + '\\\"}}"'
+
+    pyperclip.copy(curlCommand)
+
+    cprint('Success! Copied the curl command to clipboard. Now just paste this in your terminal.', 'green')
+
+    main()
+
+
+
+
+main()
